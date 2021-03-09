92635771da63c34c288e2c1bb4dfdd56ffdc19da
Deveshb15 <deveshb583@gmail.com>
2021-03-09 16:20:50 +0530

    feat: added new website link

---
+++ index.js
@@ -0,0 +1,7 @@
+const cross = document.getElementById('icon-font');
+const navbar = document.querySelector('.nav-arc');
+
+
+cross.addEventListener('click', () => {
+    navbar.style.display = 'none';
+})
